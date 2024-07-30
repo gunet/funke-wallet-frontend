6153ececb8c4d5d29ddf310e726702e6c0431d1e
kkmanos <koukoularisman@gmail.com>
2024-07-30 14:57:04 +0300

    removed redundant check url code-block

--- src/components/useCheckURL.ts
+++ src/components/useCheckURL.ts
@@ -98,19 +98,19 @@ function useCheckURL(urlToCheck: string): {
 				});
 		}
 		
-		if (Object.keys(openID4VCIClients).length && u.searchParams.get('finishUrl')) {
-			httpProxy.get(u.searchParams.get('finishUrl'), { }).then((resp) => {
-				for (const credentialIssuerIdentifier of Object.keys(openID4VCIClients)) {
-					console.log("Resp loc = ", resp.headers['location'])
-					openID4VCIClients[credentialIssuerIdentifier].handleAuthorizationResponse(resp.headers['location'])
-						.catch(err => {
-							console.error(err)
-						});
-				}
-			}).catch((err) => {
-				console.error(err)
-			})
-		}
+		// if (Object.keys(openID4VCIClients).length && u.searchParams.get('finishUrl')) {
+		// 	httpProxy.get(u.searchParams.get('finishUrl'), { }).then((resp) => {
+		// 		for (const credentialIssuerIdentifier of Object.keys(openID4VCIClients)) {
+		// 			console.log("Resp loc = ", resp.headers['location'])
+		// 			openID4VCIClients[credentialIssuerIdentifier].handleAuthorizationResponse(resp.headers['location'])
+		// 				.catch(err => {
+		// 					console.error(err)
+		// 				});
+		// 		}
+		// 	}).catch((err) => {
+		// 		console.error(err)
+		// 	})
+		// }
 		if (urlToCheck && isLoggedIn && window.location.pathname === "/cb") {
 			(async () => {
 				await communicationHandler(urlToCheck);
