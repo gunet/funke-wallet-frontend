89b12548b48679d435e6688cbb92c6e4f31659af
kkmanos <koukoularisman@gmail.com>
2024-07-31 10:29:56 +0300

    formatting changes

--- src/lib/mdl/mdl.ts
+++ src/lib/mdl/mdl.ts
@@ -7,7 +7,7 @@ const trustedCerts = JSON.parse(new TextDecoder().decode(jose.base64url.decode(p
 
 const importCert = async (cert) => {
 	// convert issuer cert to KeyLike
-	const issuerCertJose = await jose.importX509(cert, 'ES256', { extractable: true});
+	const issuerCertJose = await jose.importX509(cert, 'ES256', { extractable: true });
 	// convert issuer cert from KeyLike to JWK
 	const issuerCertJwk = await jose.exportJWK(issuerCertJose)
 	// import issuer cert from JWK to CryptoKey
@@ -28,7 +28,7 @@ export const verifyMdocWithAllCerts = async (mdoc: MDoc) => {
 		try {
 			return issuerAuth.verify(await importCert(cert));
 		}
-		catch(err) {
+		catch (err) {
 			console.error(err)
 			return false;
 		}
@@ -39,30 +39,30 @@ export const verifyMdocWithAllCerts = async (mdoc: MDoc) => {
 }
 
 export const parseMsoMdocCredential = async (mso_mdoc_cred: string, docType: string): Promise<any> => {
-	
-  const credentialBytes = jose.base64url.decode(mso_mdoc_cred);
-  const issuerSigned = await cbor.decode(credentialBytes);
-  const m = {
-    version: '1.0',
-    documents: [new Map([
-      ['docType', docType],
-      ['issuerSigned', issuerSigned]
-    ])],
-    status: 0
-  }
-  const encoded = cbor.encode(m) as Uint8Array;
-  return parse(encoded);
+
+	const credentialBytes = jose.base64url.decode(mso_mdoc_cred);
+	const issuerSigned = await cbor.decode(credentialBytes);
+	const m = {
+		version: '1.0',
+		documents: [new Map([
+			['docType', docType],
+			['issuerSigned', issuerSigned]
+		])],
+		status: 0
+	}
+	const encoded = cbor.encode(m) as Uint8Array;
+	return parse(encoded);
 }
 
 export const convertToJSONWithMaps = (obj) => {
-  return JSON.parse(JSON.stringify(obj, (key, value) => {
-    if (value instanceof Map) {
-      const obj = {};
-      for (let [k, v] of value) {
-        obj[k] = v;
-      }
-      return obj;
-    }
-    return value;
-  }));
+	return JSON.parse(JSON.stringify(obj, (key, value) => {
+		if (value instanceof Map) {
+			const obj = {};
+			for (let [k, v] of value) {
+				obj[k] = v;
+			}
+			return obj;
+		}
+		return value;
+	}));
 }
